e50e2df91d741398abf827c3cbe2b58c0bf9028f
Conrad Scherb <csch666@aucklanduni.ac.nz>
2021-04-21 20:36:41 +1200

    Update Task 3.c

--- Task 3.c
+++ Task 3.c
@@ -36,109 +36,47 @@ void blink(int target) {
   _delay_ms(100);
   PORTB &= ~(1<<target);
 }
-/*
-void handleBreach(int breachPos) {
-  breaches++;
-  blink(breachPos);
-}*/
 
+void pulsePwm(double speed) {
+  if (speed > 100) {
+    speed = 100;
+  }
+  Serial.println("Speed: ");
+  Serial.print(speed);
+  double waitPeriod = 1000-(speed*10);
+  Serial.println("Wait Period: ");
+  Serial.print(waitPeriod);
+  while(1) {
+  	PORTB |= (1<<PORTB5); 
+    _delay_ms(waitPeriod);
+    PORTB &= ~(1<<PORTB5); 
+    _delay_ms(1000-waitPeriod);
+  }
+}
 
 // LB1 (LEFT)
 ISR(INT1_vect) {
   blink(PORTB3);
+  lb1Breach = 1;
   TCCR1B |= (1 << CS12);
 }
 
 // LB2 (RIGHT)
 ISR(INT0_vect) {
-  blink(PORTB1);
-  Serial.print(elapsed);
-  elapsed = -2;
-  TCCR1B &= ~(1 << CS12);
+  if (lb1Breach == 1) {
+    blink(PORTB1);
+    TCCR1B &= ~(1 << CS12);
+    lb1Breach = 0;
+    
+    // Calculate speed
+    double time = (double)elapsed/5;
+    double velocity = 20/time;
+    Serial.println(velocity);
+    pulsePwm(velocity);
+    elapsed = -2;
+  }
 }
 
 ISR(TIMER1_COMPA_vect) {
   elapsed++;
 }
-/*
-#include <util/delay.h>
-
-#define GREEN 1<<DDB1
-#define RED 1<<DDB3
-#define OSC 1<<DDB5
-#define RED_ON PORTB |= (1<<PORTB3)
-#define GREEN_ON PORTB |= (1<<PORTB1)
-#define CONFIG_OUTPUT(arg1, arg2, arg3) (DDRB |= (arg1)|(arg2)|(arg3))
-
-volatile int breaches = 0;
-
-int main(void) {
-  Serial.begin(9600);
-  Serial.println("Start");
-
-  CONFIG_OUTPUT(GREEN, RED, OSC);
-   
-  // Set interrupts
-  EIMSK |= (1<<INT1)|(1<<INT0);
-  EICRA |= (1<<ISC11)|(1<<ISC01);
-  
-  // Timer setting
-  TCCR0A = 0;
-  TCCR0B = 0;
-  TCCR0B |= (1<<CS02);
-  TIMSK0 |= (1<<TOIE0); 
- 
-  sei();
-  
-  while (1) {
-  }
-}
-
-void blink(int target) {
-  PORTB |= (1<<target);
-  _delay_ms(100);
-  PORTB &= ~(1<<target);
-}
-
-void handleBreach(int breachPos) {
-  breaches++;
-  blink(breachPos);
-}
-
-// LB1 (LEFT)
-ISR(INT1_vect) {
-  handleBreach(PORTB3);
-}
-
-// LB2 (RIGHT)
-ISR(INT0_vect) {
-  handleBreach(PORTB1);
-}
-
-ISR(TIMER0_OVF_VECT) {
-  Serial.println("Yep!");
-}*/
-
-/*
-volatile int elapsed = 0;
-
-int main(void)
-{
-  	Serial.begin(9600);
-    OCR1A = 12499;
-    TCCR1B |= (1 << WGM12);
-    TIMSK1 |= (1 << OCIE1A);
-    TCCR1B |= (1 << CS12);
-    sei();
-
-
-    while (1);
-    {
-        // we have a working Timer
-    }
-}
-ISR (TIMER1_COMPA_vect) {
-  	elapsed++;
-}
-
-*/
