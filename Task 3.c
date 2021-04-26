82b56694b2468dfe226d1df92c1d518bdf51040f
Conrad Scherb <csch666@aucklanduni.ac.nz>
2021-04-27 11:18:17 +1200

    Finish Task 3.c

--- Task 3.c
+++ Task 3.c
@@ -1,102 +1,109 @@
 #include <util/delay.h>
+ 
+#define GREEN DDB1
+#define RED DDB3
+#define PWM DDB5
 
-#define GREEN 1<<DDB1
-#define RED 1<<DDB3
-#define OSC 1<<DDB5
-#define RED_ON PORTB |= (1<<PORTB3)
-#define GREEN_ON PORTB |= (1<<PORTB1)
-#define CONFIG_OUTPUT(arg1, arg2, arg3) (DDRB |= (arg1)|(arg2)|(arg3))
-
-volatile int elapsed = -2;
-volatile int lb1Breach = 0;
-volatile int lb2Breach = 0;
-
-volatile int lb1Breaches[10];
-volatile int lb2Breaches[10];
-volatile int elapsedTimes[10];
-volatile int lb1idx, lb2idx = 0;
-
+volatile unsigned int elapsed = 0;
+volatile double speed = 0;
+volatile int startTimes[5] = {0, 0, 0, 0, 0};
+volatile int lb1idx = 0;
+ 
 int main(void) {
-  Serial.begin(9600);
-  Serial.println("Start");
 
-  CONFIG_OUTPUT(GREEN, RED, OSC);
-  
-  for (int i = 0; i < 10; i++) {
-  	lb1Breaches[i] = 0;
-  }
-  
+  // Configure the pins to output for green+red LEDs and PWM
+  DDRB != (1<<GREEN)|(1<<RED)|(1<<PWM);
+
+  // Disable all interrupts while configuring them
   cli();
-   
-  // Set interrupts
+ 
+  // Set external interrupts
   EIMSK |= (1<<INT1)|(1<<INT0);
   EICRA |= (1<<ISC11)|(1<<ISC01);
-  
+ 
   // Using the timer with OCR1A = 1999 and a prescaler
   // of 8 causes an interrupt every 1ms
   OCR1A = 1999;
   TCCR1B |= (1 << WGM12);
   TIMSK1 |= (1 << OCIE1A);
-  
+  TCCR1B |= (1 << CS11);
+
+  // Enable the interrupts after setting them up
   sei();
-  
+ 
   while (1) {
-    // Sort out the PWM generation
-    Serial.print("ok");
-    // Find the most recent non-zero elapsed time
-    for (int i = 9; i >= 0; i--) {
-      if (elapsedTimes[i] != 0) {
-      	Serial.println(elapsedTimes[i]);
-      }
+    // Produce the PWM signal in the main run loop
+    int startTime = elapsed;
+    
+    // Turn on signal
+    PORTB |= (1<<PORTB2);
+
+    // Keep the signal on for speed*10 ms
+    while (elapsed < startTime+(int(speed)*10)) {
+      asm volatile ("nop");
+    }
+
+    // Turn off signal
+    PORTB &= ~(1<<PORTB2);
+
+    // Wait until 1 second from startTime recorded
+    while (elapsed < startTime+1000) {
+      asm volatile ("nop");
     }
   }
 }
-
+ 
 void blink(int target) {
   PORTB |= (1<<target);
   _delay_ms(100);
   PORTB &= ~(1<<target);
 }
-/*
-void pulsePwm(double speed) {
-  if (speed > 100) {
-    speed = 100;
-  }
-  double waitPeriod = 1000-(speed*10);
-  while(1) {
-  	PORTB |= (1<<PORTB5); 
-    _delay_ms(waitPeriod);
-    PORTB &= ~(1<<PORTB5); 
-    _delay_ms(1000-waitPeriod);
-  }
-}*/
 
 // LB1 (LEFT)
 ISR(INT1_vect) {
-  blink(PORTB3);
-  TCCR1B |= (1 << CS11); // Start the timer with prescaler of 8
-  lb1Breaches[lb1idx] = 1;
+  // Record the current ms count into appropriate position
+  // in startTimes
+  startTimes[lb1idx] = elapsed;
+ 
+  // Change position to write next LB1 press into
   lb1idx++;
-}
 
+  // Flash the red LED
+  blink(PORTB3);
+}
+ 
 // LB2 (RIGHT)
 ISR(INT0_vect) {
-  blink(PORTB1);
-  lb2Breaches[lb2idx] = 1;
-  lb2idx++;
-}
+  // Make sure there is a car to still pass LB2
+  if (startTimes[0] != 0) {
+    // Find the elapsed time between the first car to pass 
+    // LB1 and the current LB2 press, and calculate speed from it
+    double elapsedTime = elapsed-startTimes[0];
+   
+    // Find the speed to update PWM state 
+    speed = ((double)20/elapsedTime)*1000;
 
-ISR(TIMER1_COMPA_vect) {
-  for (int i = 0; i < 10; i++) {
-    if (lb1Breaches[i] == 1 && lb2Breaches[i] == 0) {
-      elapsedTimes[i] = elapsedTimes[i] + 1;
+    printArray(startTimes);
+   
+    // Shift the start times array to the left
+    for (int i = 0; i < 4; i++) {
+        startTimes[i] = startTimes[i+1];
     }
-  }
+    startTimes[4] = 0;
   
-  Serial.print("[");
-  for (int i = 0; i < 10; i++) {
-  	Serial.print(elapsedTimes[i]);
-    Serial.print(" ");
+    printArray(startTimes);
+   
+    // Next time LB1 is pressed, change the position
+    // on start times array to write to
+    lb1idx--;
+
+    // Flash the green LED
+    blink(PORTB1);
   }
-  Serial.print("]\n");
 }
+ 
+ISR(TIMER1_COMPA_vect) {
+  // Tick up the millisecond count on timer overflow
+  elapsed++;
+}
+ 
