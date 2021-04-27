fffd95af877ed398c24ed3911fe990c84c483a92
Conrad Scherb <csch666@aucklanduni.ac.nz>
2021-04-27 12:07:15 +1200

    Update Task2.c

--- Task 2.c
+++ Task 2.c
@@ -2,32 +2,35 @@
  
 volatile int systemState = 1;
 volatile int configMode = 0;
+volatile unsigned int elapsed = 0;
 volatile uint16_t conversionResult;
  
 int main(void) {
-  Serial.begin(9600);
-  Serial.println("start");
- 
   // Set the LED pins to output
-  DDRB |= (1<<DDB0); // White
-  DDRB |= (1<<DDB1); // Green
-  DDRB |= (1<<DDB2); // Yellow
-  DDRB |= (1<<DDB3); // Red
+  DDRB |= (1<<DDB0)|(1<<DDB1)|(1<<DDB2)|(1<<DDB3);
  
   // Set input pins
   DDRD &= ~(1<<DDD2);
+
+  // Disable interrupts before configuring them
+  cli();
  
   // Set interrupt for configuration mode change
   EIMSK |= (1<<INT0);
   EICRA |= (1<<ISC01)|(1<<ISC00);
  
-  // Set ADC interrupt
-  ADMUX = 0x0;          // input pin ADC0
-  ADMUX |= (1<<REFS0); // V_REF = AV_cc
- 
+  // Enable the ADC & conversion complete interrupt
+  ADMUX = 0x0;   
+  ADMUX |= (1<<REFS0);
   ADCSRA = 0x0;
-  ADCSRA |= (1<<ADEN)|(1<<ADIE);    // enable the ADC and enable the conversion complete interrupt 
+  ADCSRA |= (1<<ADEN)|(1<<ADIE);    
   ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS2);
+
+  // Setup a timer which triggers an overflow interrupt every 1ms
+  OCR1A = 1999;
+  TCCR1B |= (1 << WGM12);
+  TIMSK1 |= (1 << OCIE1A);
+  TCCR1B |= (1 << CS11);
  
   sei();
  
@@ -37,9 +40,7 @@ int main(void) {
  
   // Turn on the white LED
   PORTB  |= (1<<PORTB0); 
- 
-  int analogPin = 0;
- 
+  
   while (1) {
     if (ledState == 0) {
       PORTB &= ~(1<<PORTB3);
@@ -62,39 +63,57 @@ int main(void) {
  
 void configModeFSM() {
   while (configMode) {
+    // Start the ADC conversion
     conversionResult = 0;
-    ADCSRA |= (1<<ADSC);    // Now start an ADC conversion 
+    ADCSRA |= (1<<ADSC); 
+
+    // Blink the white LED with appropriate duty cycle
     for (int i = 0; i < systemState; i++) {
       PORTB |= (1<<PORTB0);
       variableDelay(500/systemState);
       PORTB &= ~(1<<PORTB0); 
       variableDelay(500/systemState);
     }
-    _delay_ms(2000);
+
+    // Wait 2 seconds until repeated (or exit early if
+    // configuration mode is exited)
+    unsigned int startTime = elapsed;
+    while (elapsed < startTime+2000 && configMode) {
+      asm volatile ("nop");
+    }
   }
 }
- 
+
+// Take advantage of the clock to setup a _delay_ms() that can
+// accept variable inputs
 void variableDelay(int ms) {
-  for (int i = 0; i < ms; i++) {
-    _delay_ms(1);
+  unsigned int startTime = elapsed;
+  while (elapsed < startTime+ms) {
+    asm volatile ("nop");
   }
 } 
  
- 
+// Toggle in and out of configuration mode
 ISR(INT0_vect) {
   configMode == 0 ? configMode = 1 : configMode = 0;
 }
- 
+
+// Convert the ADC result into a systemState
 ISR(ADC_vect) {
   conversionResult = ADC;
   if (conversionResult < 255) {
     systemState = 1;
-  } else if (conversionResult < 512) {
+  } else if (conversionResult < 511) {
     systemState = 2;
   } else if (conversionResult < 767) {
     systemState = 3;
   } else {
     systemState = 4;
   }
-  Serial.println(systemState);
 }
+
+ISR(TIMER1_COMPA_vect) {
+  // Tick up the millisecond count on timer overflow
+  elapsed++;
+}
+ 
